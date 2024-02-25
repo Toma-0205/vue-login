6ea26cf97e0ba74b5805284b61310ddfc896da4b
Toma Morishita <25winterhorse@gmail.com>
2024-02-25 16:13:01 +0900

    Initial commit

---
+++ src/views/myLogin.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h2>ログイン</h2>
+      <label for="email">Email：</label>
+      <input v-model="email" type="email" id="email">
+      <br>
+      <br>
+      <label for="password">Password：</label>
+      <input v-model="password" type="password" id="password">
+      <br><br>
+      <button @click="login">送信</button>
+    </div>
+</template>
+  
+<script>
+export default{
+    data(){
+        return {
+            email:"",
+            password:"",
+            };
+    },
+    methods:{
+        login(){
+            this.$store.dispatch('login', {
+                email: this.email,
+                password: this.password
+            });      
+            this.email = "";
+            this.password = "";
+        }
+    }
+};
+</script>
---
+++ src/views/myRegister.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h2>登録</h2>
+      <label for="email">Email：</label>
+      <input v-model="email" type="email" id="email">
+      <br>
+      <br>
+      <label for="password">Password：</label>
+      <input v-model="password" type="password" id="password">
+      <br><br>
+      <button @click="register">送信</button>
+    </div>
+</template>
+  
+<script>
+export default{
+    data(){
+        return {
+            email:"",
+            password:"",
+            };
+    },
+    methods:{
+        register(){
+            this.$store.dispatch('resister', {
+                email: this.email,
+                password: this.password
+            });
+        this.email = "";
+        this.password = "";
+        }
+    }
+};
+</script>
